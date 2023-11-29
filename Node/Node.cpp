ff06036914c33a451a92a0cd8810528b8318ce3a
leshiy2216 <cyberstanislaw2004@gmail.com>
2023-11-29 21:29:30 +0400

    Start work. Added struct Node

---
+++ Node/Node.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+template <typename T>
+struct Node
+{
+    T data;
+    Node* next;
+
+    Node(const T& value) : data(value), next(nullptr) {}
+};
+
+
+template <typename T>
+class LinkedList
+{
+private:
+
+
+public:
+
+
+};
+
+int main()
+{
+    return 0;
+}
