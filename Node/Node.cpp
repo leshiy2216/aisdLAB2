7f37e2d7a6a69d4e8e81c6b22809f19004a1a0dd
leshiy2216 <cyberstanislaw2004@gmail.com>
2023-11-30 23:49:22 +0400

    added linked list and push_tail

--- Node/Node.cpp
+++ Node/Node.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
+#include <cstdlib>
+
 
 template <typename T>
-struct Node
+struct Node // узел
 {
     T data;
     Node* next;
@@ -14,10 +16,64 @@ template <typename T>
 class LinkedList
 {
 private:
-
+    Node<T>* head;
 
 public:
+    LinkedList() : head(nullptr) {}
 
+    LinkedList(const LinkedList& other) : head(nullptr)
+    {
+        Node<T>* otherCurrent = other.head;
+        while (otherCurrent != nullptr)
+        {
+            push_tail(otherCurrent->data);
+            otherCurrent = otherCurrent->next;
+        }
+    }
+
+    LinkedList(int size) : head(nullptr)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            push_tail(rand());
+        }
+    }
+
+    ~LinkedList()
+    {
+        while (head != nullptr)
+        {
+            pop_head();
+        }
+    }
+
+    void push_tail(const T& value)
+    {
+        Node<T>* newNode = new Node<T>(value);
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node<T>* current = head;
+            while (current->next != nullptr)
+            {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    void push_tail(const LinkedList& other)
+    {
+        Node<T>* otherCurrent = other.head;
+        while (otherCurrent != nullptr)
+        {
+            push_tail(otherCurrent->data);
+            otherCurrent = otherCurrent->next;
+        }
+    }
 
 };
 
